5dd9026e47d282e9bbf201ad64497788043a53a5
Hisanobu Tomari <htomari@mail.ru>
2018-03-03 13:11:24 +0900

    Update NDK install script

--- scripts/install_ndk.sh
+++ scripts/install_ndk.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -e
 source scripts/sdkpath.sh
-DEPS="$ANDROID_NDK_HOME/installed-dependencies"
+set -e
+DEPS="$NDK_HOME/installed-dependencies"
 
 if [ ! -e "$DEPS" ]; then
-	if [ ! -d "$ANDROID_NDK_HOME" ] ; then mkdir "$ANDROID_NDK_HOME" ; fi
-	cd "$ANDROID_NDK_HOME"
+	if [ ! -d "$NDK_HOME" ] ; then mkdir "$NDK_HOME" ; fi
+	cd "$NDK_HOME"
 	echo "Downloading NDK..."
 	curl -g -o ndk.zip https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip
 	unzip -o -q ndk.zip
-	echo "Installed Android NDK at $ANDROID_NDK_HOME"
+	echo "Installed Android NDK at $NDK_HOME"
 	touch "$DEPS"
 	rm ndk.zip
 fi
--- scripts/sdkpath.sh
+++ scripts/sdkpath.sh
@@ -1,2 +1,3 @@
-export ANDROID_NDK_HOME="${HOME}/mooncal-ndk"
+export NDK_HOME="${HOME}/mooncal-ndk"
+export ANDROID_NDK_HOME=$(ls -d "$NDK_HOME"/android-ndk-* | head -n 1)
 
